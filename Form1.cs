fbe66b4fc2e64e998e9c3f419289f2578933383f
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-30 00:01:26 +0300

    Bind DataGridView to contacts data source

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ContactsBusinessLayer;
+
 
 namespace ContactsDesktopPresentation
 {
     public partial class Form1 : Form
     {
+        private void _RefreshContactsList()
+        {
+            dgvAllContacts.DataSource= clsContact.GetAllContacts();
+        }
         public Form1()
         {
             InitializeComponent();
+            _RefreshContactsList();
         }
     }
 }
