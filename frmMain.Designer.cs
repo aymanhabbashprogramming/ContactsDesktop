17cbe167414700b37f8bee6e4d5347bbd2d4652e
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-30 20:36:27 +0300

    Add main form

---
+++ frmMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ContactsDesktopPresentation
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnContactsList = new System.Windows.Forms.Button();
+            this.btnAddCountry = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnAddCountry);
+            this.panel1.Controls.Add(this.btnContactsList);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1276, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnContactsList
+            // 
+            this.btnContactsList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContactsList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContactsList.Location = new System.Drawing.Point(12, 12);
+            this.btnContactsList.Name = "btnContactsList";
+            this.btnContactsList.Size = new System.Drawing.Size(218, 32);
+            this.btnContactsList.TabIndex = 0;
+            this.btnContactsList.Text = "Contacts List";
+            this.btnContactsList.UseVisualStyleBackColor = true;
+            this.btnContactsList.Click += new System.EventHandler(this.btnContactsList_Click);
+            // 
+            // btnAddCountry
+            // 
+            this.btnAddCountry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCountry.Location = new System.Drawing.Point(236, 12);
+            this.btnAddCountry.Name = "btnAddCountry";
+            this.btnAddCountry.Size = new System.Drawing.Size(218, 32);
+            this.btnAddCountry.TabIndex = 1;
+            this.btnAddCountry.Text = "Add Country";
+            this.btnAddCountry.UseVisualStyleBackColor = true;
+            this.btnAddCountry.Click += new System.EventHandler(this.btnAddCountry_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1276, 540);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IsMdiContainer = true;
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAddCountry;
+        private System.Windows.Forms.Button btnContactsList;
+    }
+}
