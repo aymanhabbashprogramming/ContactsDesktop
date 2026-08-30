43f05d5c8d1a79de15080f22d7b082d6114e7743
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-30 20:13:36 +0300

    Add country form and handle country creation

---
+++ frmAddEditCountry.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ContactsDesktopPresentation
+{
+    partial class frmAddEditCountry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMode = new System.Windows.Forms.Label();
+            this.txtPhoneCode = new System.Windows.Forms.TextBox();
+            this.txtCountryCode = new System.Windows.Forms.TextBox();
+            this.txtCountryName = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCountryID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMode.Location = new System.Drawing.Point(139, 9);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(57, 23);
+            this.lblMode.TabIndex = 1;
+            this.lblMode.Text = "Mode";
+            // 
+            // txtPhoneCode
+            // 
+            this.txtPhoneCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhoneCode.Location = new System.Drawing.Point(170, 181);
+            this.txtPhoneCode.Multiline = true;
+            this.txtPhoneCode.Name = "txtPhoneCode";
+            this.txtPhoneCode.Size = new System.Drawing.Size(234, 33);
+            this.txtPhoneCode.TabIndex = 23;
+            // 
+            // txtCountryCode
+            // 
+            this.txtCountryCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountryCode.Location = new System.Drawing.Point(170, 133);
+            this.txtCountryCode.Multiline = true;
+            this.txtCountryCode.Name = "txtCountryCode";
+            this.txtCountryCode.Size = new System.Drawing.Size(234, 33);
+            this.txtCountryCode.TabIndex = 22;
+            // 
+            // txtCountryName
+            // 
+            this.txtCountryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountryName.Location = new System.Drawing.Point(170, 86);
+            this.txtCountryName.Multiline = true;
+            this.txtCountryName.Name = "txtCountryName";
+            this.txtCountryName.Size = new System.Drawing.Size(234, 33);
+            this.txtCountryName.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 23);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Phone Code";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 23);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Country Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 23);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Country Name";
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(227, 236);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(90, 36);
+            this.btnClose.TabIndex = 28;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(79, 236);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(142, 36);
+            this.btnSave.TabIndex = 27;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 23);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Country ID : ";
+            // 
+            // lblCountryID
+            // 
+            this.lblCountryID.AutoSize = true;
+            this.lblCountryID.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountryID.Location = new System.Drawing.Point(139, 53);
+            this.lblCountryID.Name = "lblCountryID";
+            this.lblCountryID.Size = new System.Drawing.Size(38, 23);
+            this.lblCountryID.TabIndex = 30;
+            this.lblCountryID.Text = "----";
+            // 
+            // frmAddEditCountry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(432, 284);
+            this.Controls.Add(this.lblCountryID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPhoneCode);
+            this.Controls.Add(this.txtCountryCode);
+            this.Controls.Add(this.txtCountryName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmAddEditCountry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmAddEditCountry";
+            this.Load += new System.EventHandler(this.frmAddCountry_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.TextBox txtPhoneCode;
+        private System.Windows.Forms.TextBox txtCountryCode;
+        private System.Windows.Forms.TextBox txtCountryName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCountryID;
+    }
+}
