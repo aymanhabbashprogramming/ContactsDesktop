79d5b88abd81c22486033c1710c1ab85efc4bbcc
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-30 17:24:26 +0300

    Fix contact information saving

--- frmAddEditContact.cs
+++ frmAddEditContact.cs
@@ -39,6 +39,8 @@ namespace ContactsDesktopPresentation
             {
                 _Mode = enMode.Update;
             }
+
+            btnRemoveImage.Visible = (pbContactImage.Image != null);
         }
         private void frmAddEditContact_Load(object sender, EventArgs e)
         {
@@ -81,12 +83,70 @@ namespace ContactsDesktopPresentation
                 pbContactImage.Load(_Contact.ImagePath);
             }
 
-            btnRemoveImage.Visible = (_Contact.ImagePath != "");
+            btnRemoveImage.Visible = !string.IsNullOrEmpty(_Contact.ImagePath);
             cbCountries.SelectedIndex= cbCountries.FindString(clsCountry.Find(_ContactID).CountryName);
         }
 
+        private void btnSetImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Process the selected file
+                string selectedFilePath = openFileDialog1.FileName;
+                //MessageBox.Show("Selected Image is:" + selectedFilePath);
+
+                pbContactImage.Load(selectedFilePath);
+                btnRemoveImage.Visible = (pbContactImage.Image != null);
+
+            }
+        }
+
+        private void btnRemoveImage_Click(object sender, EventArgs e)
+        {
+            pbContactImage.ImageLocation = null;
+            btnRemoveImage.Visible = false;
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int NewCountryID = clsCountry.Find(cbCountries.Text).ID;
+
+            _Contact.FirstName = txtFirstName.Text.Trim();
+            _Contact.LastName = txtLastName.Text.Trim();
+            _Contact.Email = txtEmail.Text.Trim();
+            _Contact.Phone = txtPhone.Text.Trim();
+            _Contact.Address = txtAddress.Text.Trim();
+            _Contact.DateOfBirth = dtpDateOfBirth.Value;
+            _Contact.CountryID = NewCountryID;
+
+            if (pbContactImage.ImageLocation != null)
+                _Contact.ImagePath = pbContactImage.ImageLocation;
+            else
+                _Contact.ImagePath = "";
 
 
+            if (_Contact.Save())
+            {
+                MessageBox.Show("Data Saved Successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Error: Data Is not Saved Successfully.");
+            }
 
+            _Mode = enMode.Update;
+            lblMode.Text = "Edit Contact ID = " + _Contact.ID;
+            lblContactID.Text = _Contact.ID.ToString();
+        }
     }
 }
